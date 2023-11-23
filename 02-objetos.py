8c713a6aaeb8e053e82552f2b203509383b727b0
Mariana Leoni <leonimariana@gmail.com>
2023-11-23 19:37:26 -0300

    elimine la clase catalogo

--- 02-objetos.py
+++ 02-objetos.py
@@ -1,5 +1,8 @@
-def agregar_producto(codigo,foto,titulo,descripcion,ambientes,habitaciones,baños,telefono):
-    if consultar_producto(codigo):
+class Catalogo:
+    productos = []
+
+def agregar_producto(self,codigo,foto,titulo,descripcion,ambientes,habitaciones,baños,telefono):
+    if self.consultar_producto(codigo):
         return False
     nuevo_producto={
         'codigo':codigo,
@@ -11,17 +14,17 @@ def agregar_producto(codigo,foto,titulo,descripcion,ambientes,habitaciones,baño
         'baños':baños,
         'telefono':telefono
     }
-    productos.append(nuevo_producto)
+    self.productos.append(nuevo_producto)
     return True
 
-def consultar_producto(codigo):
-    for producto in productos:
+def consultar_producto(self,codigo):
+    for producto in self.productos:
         if producto['codigo'] == codigo:
             return producto
     return False
 
-def modificar_producto(codigo, nueva_foto, nuevo_titulo, nueva_descripcion, nuevo_ambientes, nueva_habitaciones, nuevo_baños, nuevo_telefono):
-    for producto in productos:
+def modificar_producto(self, codigo, nueva_foto, nuevo_titulo, nueva_descripcion, nuevo_ambientes, nueva_habitaciones, nuevo_baños, nuevo_telefono):
+    for producto in self.productos:
         if producto['codigo'] == codigo:
             producto['foto'] = nueva_foto,
             producto['titulo']= nuevo_titulo,
@@ -33,9 +36,9 @@ def modificar_producto(codigo, nueva_foto, nuevo_titulo, nueva_descripcion, nuev
         return True
     return False
 
-def listar_productos():
+def listar_productos(self):
     print('-' * 50)
-    for producto in productos:
+    for producto in self.productos:
         print(f"Código.......:{producto['codigo']}")
         print(f"Foto.........:{producto['foto']}")
         print(f"Título.......:{producto['titulo']}")
@@ -46,16 +49,25 @@ def listar_productos():
         print(f"Teléfono.....:{producto['telefono']}")
         print('-' * 50)
 
-def eliminar_producto(codigo):
-    for producto in productos:
+def eliminar_producto(self,codigo):
+    for producto in self.productos:
         if producto['codigo']== codigo:
-            productos.remove(producto)
+            self.productos.remove(producto)
         return True
     return False
 
-
-   
-
-productos = []
-
-    
+def mostrar_producto(self, codigo):
+    producto= self.consultar_producto(codigo)
+    if producto:
+        print('-' * 50)
+        print(f"Código.......:{producto['codigo']}")
+        print(f"Foto.........:{producto['foto']}")
+        print(f"Título.......:{producto['titulo']}")
+        print(f"Descripción..:{producto['descripcion']}")
+        print(f"Ambientes....:{producto['ambientes']}")
+        print(f"Habitaciones.:{producto['habitaciones']}")
+        print(f"Baños........:{producto['baños']}")
+        print(f"Teléfono.....:{producto['telefono']}")
+        print('-' * 50)
+    else:
+        print("Producto no encontrado.")
