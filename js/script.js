a5a068390ff3a626a88af0e2b3ae2364f60e6f13
Franco Pericotti <fpericotti@outlook.com>
2023-12-03 00:29:23 -0300

    Carga de archivos, creacion de carpetas y css para altas

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//Header for JavaScript
+var cad = `
+<div class="flex-containerHeader">
+    <a class="imgHB"><img src="img/LogoHB.svg" alt="Casa Alquileres"></img></a>
+    <h1>Book.your.Home</h1>
+
+</div>
+`
+document.getElementById("idHeader").innerHTML = cad
+
+
+//Footer for JavaScript
+cad = `
+<div class="flex-footer">
+    <p class="derechos">Derechos reservados @2020</p>
+</div>
+`
+
+document.getElementById("idFooter").innerHTML=cad
+
