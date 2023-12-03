eb5aad960eabed287b4fe6c66e2c327e14ffc395
GuidoPericotti <guido.pericotti@gmail.com>
2023-12-03 18:18:21 -0300

    Update web 2

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var cad = `
 <div class="flex-containerHeader">
     <a class="imgHB"><img src="img/LogoHB.svg" alt="Casa Alquileres"></img></a>
     <h1>Book.your.Home</h1>
-    <a style="text-decoration: none; color: white; display: flex; width: 5rem; padding: 2%;" href="../CacFullStackPython/index.html">Volver</a>
+    <a style="text-decoration: none; color: white; display: flex; width: 5rem; padding: 2%;" href="/web/index.html">Volver</a>
 </div>
 `
 document.getElementById("idHeader").innerHTML = cad
