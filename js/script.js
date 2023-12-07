c9e632dc762dda574e4dc8bcc540ee54986ace48
Franco Pericotti <fpericotti@outlook.com>
2023-12-07 17:48:57 -0300

    cambios js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 //Header for JavaScript
 var cad = `
-<div class="flex-containerHeader">
-    <a class="imgHB" href="href="https://fpericotti.github.io/proyecto/web/index.html"><img src="img/LogoHB.svg" alt="Casa Alquileres"></img></a>
-    <h1>Book.your.Home</h1>
-    <a style="text-decoration: none; color: white; display: flex; width: 5rem; padding: 2%;" href="https://fpericotti.github.io/proyecto/web/index.html">Volver</a>
-</div>
-`
+    <div class="flex-containerHeader">
+        <a class="imgHB" href="href="https://fpericotti.github.io/proyecto/web/index.html"><img src="img/LogoHB.svg" alt="Casa Alquileres"></img></a>
+        <h1>Book.your.Home</h1>
+        <a style="text-decoration: none; color: white; display: flex; width: 5rem; padding: 2%;" href="https://fpericotti.github.io/proyecto/web/index.html">Volver</a>
+    </div>
+    `
 document.getElementById("idHeader").innerHTML = cad
 
 
 //Footer for JavaScript
 cad = `
-<div class="flex-footer">
-    <p class="derechos">Derechos reservados @2020</p>
-</div>
-`
+    <div class="flex-footer">
+        <p class="derechos">Derechos reservados @2020</p>
+    </div>
+    `
 
 document.getElementById("idFooter").innerHTML=cad
 
